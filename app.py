3be5555d03e2c919309005713a83f4ac4dea8828
maars202 <85531823+maars202@users.noreply.github.com>
2022-11-02 23:04:25 +0800

    automated testing workflow

--- app.py
+++ app.py
@@ -18,33 +18,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-
-# # CREATE A users TABLE USING RAW SQL QUERY
-# db.engine.execute(
-#     '''
-#     CREATE TABLE `Role` (
-#     `role_id` int PRIMARY KEY AUTO_INCREMENT,
-#     `role_name` varchar(20) NOT NULL);
-#     '''
-# )
-  
-
-
-# # CREATE A users TABLE USING RAW SQL QUERY
-# db.engine.execute(
-#     '''
-#     CREATE TABLE `Staff` (
-#         `staff_id` int PRIMARY KEY,
-#         `staff_Fname` varchar(50) NOT NULL,
-#         `staff_Lname` varchar(50) NOT NULL,
-#         `department` varchar(50) NOT NULL,
-#         `email` varchar(50) NOT NULL,
-#         `role` int NOT NULL
-#     );
-#     '''
-# )
-  
-
 CORS(app)
 
 

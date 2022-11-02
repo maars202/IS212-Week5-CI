d8a775467ee95fa478f7ed751305fd1732ff3741
maars202 <85531823+maars202@users.noreply.github.com>
2022-11-02 16:09:58 +0800

    flow

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.app_context().push()
 
 if __name__ == '__main__':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root' + \
